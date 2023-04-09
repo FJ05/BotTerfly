554b43f25438b56ad18a5685061d51cbcdd74f49
Filip Jansson <fredrikfilip.fj@gmail.com>
2023-04-09 16:34:47 +0200

    add info command

---
+++ commands/info.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('info')
+		.setDescription('Sends info about the bot'),
+	async execute(interaction) {
+		await interaction.reply('This is a certified hood classic bot.');
+	},
+};
