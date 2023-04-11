b33b099e5c098da1efc56769a9e767d3b49e3d49
Filip Jansson <fredrikfilip.fj@gmail.com>
2023-04-11 02:26:58 +0200

    now running oobabooga/text-generation-webui

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token, clientId } = require('./config.json');
 const { prompt } = require('./prompt.json')
+const axios = require('axios');
 // Create a new client instance
 const client = new Client({
     intents: [
@@ -18,6 +19,11 @@ client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+// Networking settings¨
+var oobServer = "127.0.0.1"
+var oobPort = 7860;
+
+
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
@@ -28,6 +34,7 @@ for (const file of commandFiles) {
 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
+
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
@@ -57,50 +64,62 @@ client.on(Events.InteractionCreate, async interaction => {
 });
 
 // array of strings that i don't want in my output
-var badWords = ["<start>", "<end>", "undefined", "[end of text]", "you respond with:", "says:", "bot says:", "\""];
+var badWords = ["<start>", "<end>", "undefined", "[end of text]", "you respond with:", "says:", "BotTerfly says:", "\""];
 var activce = false;
+
+// config for the web ui
+
 // bot replies to messages if it is mentioned
+var thinking = false;
 client.on("messageCreate", (message) => {
     if (message.author.bot) return false;
     // if the message contains the bot's client ID, reply with a mention
-    if (message.content.includes(clientId)) {
+    if (message.content.includes(clientId) && !thinking) {
+		thinking = true;
+		// if thinking is true repeat message.channel.sendTyping();
+		message.channel.sendTyping();
         var content = message.content;
+		// remove the bot's client ID from the message
+		//connect to oobaboogas server
+		var url = "http://" + oobServer + ":" + oobPort + "/run/textgen";
+		var prompt = content;
+		const params = {
+			'max_new_tokens': 120,
+			'do_sample': true,
+			'temperature': 0.72,
+			'top_p': 0.73,
+			'typical_p': 1,
+			'repetition_penalty': 1.18,
+			'encoder_repetition_penalty': 1.0,
+			'top_k': 0,
+			'min_length': 0,
+			'no_repeat_ngram_size': 0,
+			'num_beams': 1,
+			'penalty_alpha': 0,
+			'length_penalty': 1,
+			'early_stopping': false,
+			'seed': -1,
+		};
+		
+		const payload = JSON.stringify([prompt, params]);
+		const requestData = { data: [payload] };
+	
+		console
+		axios.post(url, requestData)
+		.then(response => {
+			// Handle the response
+			var botResponse = response.data.data[0];
+			// remove the prompt from the response
+			botResponse = botResponse.replace(content, "");
 
-        // remove the bot's client ID from the message
-        if (content.includes("<@1094580903457603714>")) {
-            content = content.replace("<@1094580903457603714>","");
-        }
-        // content will be used as a prompt for the chatbot
-        if (!activce) {
-            activce = true;
-            const Dalai = require('dalai')
-            try {
-                var responce;
-                new Dalai().request({
-                    model: "alpaca.7B",
-                    n_predict: 128,
-                    threads: -2,
-                    prompt: prompt + "\n" + message.author.username + " says: " + content + "\nbot says: ",
-                    }, (token) => {
-                        responce += token;
-                        process.stdout.write(token)
-                            if (responce.includes("<end>")) {
-                                responce = responce.replace(prompt, "");
-                                responce = responce.replace(message.author.username, "");
-                                responce = responce.replace(content, "");
-                                // remove bad words from the output
-                                for (var i = 0; i < badWords.length; i++) {
-                                    responce = responce.replace(badWords[i], "");
-                                }
-                                message.reply(responce);
-                                activce = false;
-                            }
-                    })
-            } catch (error) {
-                console.error(error);
-            }  
-        }
-        
+			message.reply(botResponse);
+			thinking = false;
+		})
+		.catch(error => {
+			// Handle errors
+			console.error('Error:', error);
+			thinking = false;
+		});
     }
 });
 // Log in to Discord with your client's token
